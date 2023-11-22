c1e74da973eef9ff90376cf162c93bd2412837d2
Weston de Lomba <149288173+wetstn@users.noreply.github.com>
2023-11-21 22:20:25 -0500

    Update epilepsy_summary_stats.r

--- epilepsy_summary_stats.r
+++ epilepsy_summary_stats.r
@@ -10,15 +10,45 @@ library(googlesheets4)
 library(dplyr)
 library(finalfit)
 
-epilepsy_df <- read_sheet("https://docs.google.com/spreadsheets/d/1v4m3ONAJY-QX06uH49JpdSuSuA7hWcYDBPBTBDRlD3Y/edit?usp=sharing", sheet = "NSQIP_Epilepsy_CPT_ICD_2010_202")
+epilepsy_df <- read_sheet("https://docs.google.com/spreadsheets/d/1UTFaBQiaD4vyVr1mPJwzCm-patal6hx6ORAGal_RAsQ/edit?usp=sharing", sheet = "NSQIP_Epilepsy_CPT_ICD_2010_2020")
 
-# Set numeric variables 
-numeric_variables <- c("AGE", "height", "weight", "packs", "MORTPROB", "MORBPROB",
-                        "anesthesia_time", "op_time", "hlos", "READMPODAYS1")
+# Set numeric variables
+numeric_variables <- c(
+  "CASEID", "WORKRVU", "AGE", "ADMYR", "ADMSYR", "OPERYR", "HEIGHT", "WEIGHT",
+  "PACKS", "DPRNA", "DPRBUN", "DPRCREAT", "DPRALBUM", "DPRBILI", "DPRSGOT",
+  "DPRALKPH", "DPRWBC", "DPRHCT", "DPRPLATE", "DPRPTT", "DPRPT", "DPRINR",
+  "PRSODM", "PRBUN", "PRCREAT", "PRALBUM", "PRBILI", "PRSGOT", "PRALKPH",
+  "PRWBC", "PRHCT", "PRPLATE", "PRPTT", "PRINR", "PRPT", "PGY", "MALLAMP",
+  "MORTPROB", "MORBPROB", "RBC", "ANESURG", "SURGANE", "DPATRM", "ANETIME",
+  "OPTIME", "SDISDT", "HDISDT", "YRDEATH", "TOTHLOS", "ADMQTR", "HTOODAY",
+  "STOODAY", "TOTSLOS", "NSUPINFEC", "DSUPINFEC", "NWNDINFD", "DWNDINFD",
+  "NORGSPCSSI", "NDEHIS", "DDEHIS", "NOUPNEUMO", "DOUPNEUMO", "NREINTUB",
+  "DREINTUB", "NPULEMBOL", "DPULEMBOL", "NFAILWEAN", "DFAILWEAN", "NRENAINSF",
+  "DRENAINSF", "NOPRENAFL", "DOPRENAFL", "NURNINFEC", "DURNINFEC", "NCNSCVA",
+  "DCNSCVA", "NCNSCOMA", "DCNSCOMA", "NNEURODEF", "DNEURODEF", "NCDARREST",
+  "DCDARREST", "NCDMI", "DCDMI", "NOTHBLEED", "DOTHBLEED", "NOTHGRAFL",
+  "DOTHGRAFL", "NOTHDVT", "DOTHDVT", "NOTHSYSEP", "DOTHSYSEP", "NOTHSESHOCK",
+  "DOTHSESHOCK", "DSDTOHD", "DOPERTOD", "DOPTODIS", "RETORPODAYS",
+  "RETOR2PODAYS", "READMPODAYS1", "READMPODAYS2", "READMPODAYS3",
+  "READMPODAYS4", "READMPODAYS5", "NOTHCDIFF", "DOTHCDIFF", "BLEED_UNITS_TOT",
+  "OTHERWRVU", "CONWRVU"
+)
+
+# Convert -99 to NA for all numeric_variables
+epilepsy_df[numeric_variables][epilepsy_df[numeric_variables] == -99] <- NA
 
 # Update numeric data class type
 epilepsy_df <- epilepsy_df %>%
   mutate(across(all_of(numeric_variables), ~as.numeric(as.character(.))))
+
+# Calculate BMI and insert it as the 19th column
+epilepsy_df <- cbind(epilepsy_df[, 1:18], BMI = with(epilepsy_df, 703 * WEIGHT / ((HEIGHT)^2)), epilepsy_df[, 19:ncol(epilepsy_df)])
+
+numeric_variables <- c(
+  numeric_variables[1:8],
+  "BMI",
+  numeric_variables[9:length(numeric_variables)]
+)
 
 # ----------------------------------------------------------------------
 # Construct summary tables for race and sex using finalfit
@@ -27,17 +57,38 @@ epilepsy_df <- epilepsy_df %>%
 # Summary table for race 
 
 # Set the dependent variable
-dependent = "race"
+dependent = "RACE_NEW"
 
 # Set explanatory variables
-explanatory <- c("sex", "inpatient_outpatient", "AGE", "height", "weight", "diabetes", "smoking", "packs",
-                 "EtOH", "dyspnea", "functional_status", "HXCOPD", "CPNEUMON", "HXCHF", "HXMI", "HYPERMED",
-                 "HXPVD", "HEMI", "CVA", "tumor_cns", "steroid", "bleeding_disorder", "wound_class", "asa_class",
-                 "MORTPROB", "MORBPROB", "anesthesia_time", "op_time", "hlos", "superficial_ssi", "wound_infection",
-                 "organ_space_ssi", "dehiscience", "pneumonia", "reintubation", "PE", "failed_wean", "uti", "cva",
-                 "coma", "deficit", "MI", "dvt", "septic_shock", "return_to_OR", "ELECTSURG", "discharge",
-                 "unplanned_readmission", "reoperation", "READMPODAYS1", "READMPODAYS2", "READMPODAYS3",
-                 "READMPODAYS4", "cdiff", "UNPLANNEDREADMISSION")
+explanatory <- c(
+  "SEX", "ETHNICITY_HISPANIC", "PRNCPTX", "CPT", "WORKRVU", "INOUT",
+  "TRANST", "AGE", "ADMYR", "ADMSYR", "OPERYR", "ANESTHES", "ATTEND", "SURGSPEC", "HEIGHT",
+  "WEIGHT", "BMI", "DIABETES", "SMOKE", "PACKS", "ETOH", "DYSPNEA", "DNR", "FNSTATUS1", "FNSTATUS2",
+  "VENTILAT", "HXCOPD", "CPNEUMON", "ASCITES", "ESOVAR", "HXCHF", "HXMI", "PRVPCI", "PRVPCS",
+  "HXANGINA", "HYPERMED", "HXPVD", "RESTPAIN", "RENAFAIL", "DIALYSIS", "IMPSENS", "COMA",
+  "HEMI", "HXTIA", "CVA", "CVANO", "TUMORCNS", "PARA", "QUAD", "DISCANCR", "WNDINF", "STEROID",
+  "WTLOSS", "BLEEDDIS", "TRANSFUS", "CHEMO", "RADIO", "PRSEPIS", "PREGNANCY", "PROPER30", "DPRNA",
+  "DPRBUN", "DPRCREAT", "DPRALBUM", "DPRBILI", "DPRSGOT", "DPRALKPH", "DPRWBC", "DPRHCT", "DPRPLATE",
+  "DPRPTT", "DPRPT", "DPRINR", "PRSODM", "PRBUN", "PRCREAT", "PRALBUM", "PRBILI", "PRSGOT", "PRALKPH",
+  "PRWBC", "PRHCT", "PRPLATE", "PRPTT", "PRINR", "PRPT", "OPNOTE", "PGY", "EMERGNCY", "WNDCLAS", "ASACLAS",
+  "AIRTRA", "MALLAMP", "MORTPROB", "MORBPROB", "RBC", "ANESURG", "SURGANE", "DPATRM", "ANETIME", "OPTIME",
+  "TYPEINTOC", "SDISDT", "HDISDT", "YRDEATH", "TOTHLOS", "ADMQTR", "HTOODAY", "STOODAY", "TOTSLOS",
+  "NSUPINFEC", "SUPINFEC", "DSUPINFEC", "NWNDINFD", "WNDINFD", "DWNDINFD", "NORGSPCSSI", "ORGSPCSSI",
+  "DORGSPCSSI", "NDEHIS", "DEHIS", "DDEHIS", "NOUPNEUMO", "OUPNEUMO", "DOUPNEUMO", "NREINTUB", "REINTUB",
+  "DREINTUB", "NPULEMBOL", "PULEMBOL", "DPULEMBOL", "NFAILWEAN", "FAILWEAN", "DFAILWEAN", "NRENAINSF",
+  "RENAINSF", "DRENAINSF", "NOPRENAFL", "OPRENAFL", "DOPRENAFL", "NURNINFEC", "URNINFEC", "DURNINFEC",
+  "NCNSCVA", "CNSCVA", "DCNSCVA", "NCNSCOMA", "CNSCOMA", "DCNSCOMA", "NNEURODEF", "NEURODEF", "DNEURODEF",
+  "NCDARREST", "CDARREST", "DCDARREST", "NCDMI", "CDMI", "DCDMI", "NOTHBLEED", "OTHBLEED", "DOTHBLEED",
+  "NOTHGRAFL", "OTHGRAFL", "DOTHGRAFL", "NOTHDVT", "OTHDVT", "DOTHDVT", "NOTHSYSEP", "OTHSYSEP", "DOTHSYSEP",
+  "NOTHSESHOCK", "OTHSESHOCK", "DOTHSESHOCK", "PODIAG", "PODIAGTX", "RETURNOR", "DSDTOHD", "DOPERTOD", "DOPTODIS",
+  "OTHERCPT", "OTHERPROC", "CONCPT", "CONCURR", "ELECTSURG", "DISCHDEST", "READMISSION", "UNPLANREADMISSION",
+  "REOPERATION", "STILLINHOSP", "SSSIPATOS", "DSSIPATOS", "OSSIPATOS", "PNAPATOS", "UTIPATOS", "VENTPATOS",
+  "SEPSISPATOS", "SEPSHOCKPATOS", "RETORPODAYS", "RETORRELATED", "RETOR2PODAYS", "RETOR2RELATED", "READMPODAYS1",
+  "READMPODAYS2", "READMPODAYS3", "READMPODAYS4", "READMPODAYS5", "PODIAG10", "PODIAGTX10", "WOUND_CLOSURE",
+  "PODIAG_OTHER", "PODIAG_OTHER10", "ANESTHES_OTHER", "PUFYEAR", "OTHCDIFF", "NOTHCDIFF", "DOTHCDIFF", "EOL_WDCARE",
+  "BLEED_UNITS_TOT", "OTHERWRVU", "CONWRVU", "UNPLANNEDREADMISSION", "REOPORCPT", "REOPORICD9", "REOPORICD10",
+  "READMSUSPREASON", "READMRELATED", "READMRELICD9", "READMRELICD10", "READMUNRELSUSP", "READMUNRELICD9", "READMUNRELICD10"
+)
 
 # Generate summary table for race
 epilepsy_summary_race <- epilepsy_df %>% 
@@ -50,7 +101,7 @@ epilepsy_summary_race <- epilepsy_df %>%
                      p = FALSE, 
                      cont = "mean",
                      cont_cut = 2,
-                     orderbytotal = TRUE, 
+                     orderbytotal = FALSE, 
                      include_row_totals_percent = TRUE,
                      digits = c(1, 1, 3, 1, 0)
   )
@@ -58,17 +109,38 @@ epilepsy_summary_race <- epilepsy_df %>%
 # Summary table for sex
 
 # Set dependent variable
-dependent = "sex"
+dependent = "SEX"
 
 # Set explanatory variables
-explanatory <- c("race", "inpatient_outpatient", "AGE", "height", "weight", "diabetes", "smoking", "packs",
-                 "EtOH", "dyspnea", "functional_status", "HXCOPD", "CPNEUMON", "HXCHF", "HXMI", "HYPERMED",
-                 "HXPVD", "HEMI", "CVA", "tumor_cns", "steroid", "bleeding_disorder", "wound_class", "asa_class",
-                 "MORTPROB", "MORBPROB", "anesthesia_time", "op_time", "hlos", "superficial_ssi", "wound_infection",
-                 "organ_space_ssi", "dehiscience", "pneumonia", "reintubation", "PE", "failed_wean", "uti", "cva",
-                 "coma", "deficit", "MI", "dvt", "septic_shock", "return_to_OR", "ELECTSURG", "discharge",
-                 "unplanned_readmission", "reoperation", "READMPODAYS1", "READMPODAYS2", "READMPODAYS3",
-                 "READMPODAYS4", "cdiff", "UNPLANNEDREADMISSION")
+explanatory <- c(
+  "RACE_NEW", "ETHNICITY_HISPANIC", "PRNCPTX", "CPT", "WORKRVU", "INOUT",
+  "TRANST", "AGE", "ADMYR", "ADMSYR", "OPERYR", "ANESTHES", "ATTEND", "SURGSPEC", "HEIGHT",
+  "WEIGHT", "BMI", "DIABETES", "SMOKE", "PACKS", "ETOH", "DYSPNEA", "DNR", "FNSTATUS1", "FNSTATUS2",
+  "VENTILAT", "HXCOPD", "CPNEUMON", "ASCITES", "ESOVAR", "HXCHF", "HXMI", "PRVPCI", "PRVPCS",
+  "HXANGINA", "HYPERMED", "HXPVD", "RESTPAIN", "RENAFAIL", "DIALYSIS", "IMPSENS", "COMA",
+  "HEMI", "HXTIA", "CVA", "CVANO", "TUMORCNS", "PARA", "QUAD", "DISCANCR", "WNDINF", "STEROID",
+  "WTLOSS", "BLEEDDIS", "TRANSFUS", "CHEMO", "RADIO", "PRSEPIS", "PREGNANCY", "PROPER30", "DPRNA",
+  "DPRBUN", "DPRCREAT", "DPRALBUM", "DPRBILI", "DPRSGOT", "DPRALKPH", "DPRWBC", "DPRHCT", "DPRPLATE",
+  "DPRPTT", "DPRPT", "DPRINR", "PRSODM", "PRBUN", "PRCREAT", "PRALBUM", "PRBILI", "PRSGOT", "PRALKPH",
+  "PRWBC", "PRHCT", "PRPLATE", "PRPTT", "PRINR", "PRPT", "OPNOTE", "PGY", "EMERGNCY", "WNDCLAS", "ASACLAS",
+  "AIRTRA", "MALLAMP", "MORTPROB", "MORBPROB", "RBC", "ANESURG", "SURGANE", "DPATRM", "ANETIME", "OPTIME",
+  "TYPEINTOC", "SDISDT", "HDISDT", "YRDEATH", "TOTHLOS", "ADMQTR", "HTOODAY", "STOODAY", "TOTSLOS",
+  "NSUPINFEC", "SUPINFEC", "DSUPINFEC", "NWNDINFD", "WNDINFD", "DWNDINFD", "NORGSPCSSI", "ORGSPCSSI",
+  "DORGSPCSSI", "NDEHIS", "DEHIS", "DDEHIS", "NOUPNEUMO", "OUPNEUMO", "DOUPNEUMO", "NREINTUB", "REINTUB",
+  "DREINTUB", "NPULEMBOL", "PULEMBOL", "DPULEMBOL", "NFAILWEAN", "FAILWEAN", "DFAILWEAN", "NRENAINSF",
+  "RENAINSF", "DRENAINSF", "NOPRENAFL", "OPRENAFL", "DOPRENAFL", "NURNINFEC", "URNINFEC", "DURNINFEC",
+  "NCNSCVA", "CNSCVA", "DCNSCVA", "NCNSCOMA", "CNSCOMA", "DCNSCOMA", "NNEURODEF", "NEURODEF", "DNEURODEF",
+  "NCDARREST", "CDARREST", "DCDARREST", "NCDMI", "CDMI", "DCDMI", "NOTHBLEED", "OTHBLEED", "DOTHBLEED",
+  "NOTHGRAFL", "OTHGRAFL", "DOTHGRAFL", "NOTHDVT", "OTHDVT", "DOTHDVT", "NOTHSYSEP", "OTHSYSEP", "DOTHSYSEP",
+  "NOTHSESHOCK", "OTHSESHOCK", "DOTHSESHOCK", "PODIAG", "PODIAGTX", "RETURNOR", "DSDTOHD", "DOPERTOD", "DOPTODIS",
+  "OTHERCPT", "OTHERPROC", "CONCPT", "CONCURR", "ELECTSURG", "DISCHDEST", "READMISSION", "UNPLANREADMISSION",
+  "REOPERATION", "STILLINHOSP", "SSSIPATOS", "DSSIPATOS", "OSSIPATOS", "PNAPATOS", "UTIPATOS", "VENTPATOS",
+  "SEPSISPATOS", "SEPSHOCKPATOS", "RETORPODAYS", "RETORRELATED", "RETOR2PODAYS", "RETOR2RELATED", "READMPODAYS1",
+  "READMPODAYS2", "READMPODAYS3", "READMPODAYS4", "READMPODAYS5", "PODIAG10", "PODIAGTX10", "WOUND_CLOSURE",
+  "PODIAG_OTHER", "PODIAG_OTHER10", "ANESTHES_OTHER", "PUFYEAR", "OTHCDIFF", "NOTHCDIFF", "DOTHCDIFF", "EOL_WDCARE",
+  "BLEED_UNITS_TOT", "OTHERWRVU", "CONWRVU", "UNPLANNEDREADMISSION", "REOPORCPT", "REOPORICD9", "REOPORICD10",
+  "READMSUSPREASON", "READMRELATED", "READMRELICD9", "READMRELICD10", "READMUNRELSUSP", "READMUNRELICD9", "READMUNRELICD10"
+)
 
 # Generate summary table for sex
 epilepsy_summary_sex <- epilepsy_df %>%
@@ -81,26 +153,26 @@ epilepsy_summary_sex <- epilepsy_df %>%
                      p = FALSE,
                      cont = "mean",
                      cont_cut = 2,
-                     orderbytotal = TRUE,
+                     orderbytotal = FALSE,
                      include_row_totals_percent = TRUE,
                      digits = c(1, 1, 3, 1, 0)
   )
 
 # Write the summary data frames to the shared Google Drive
-#write_sheet(epilepsy_summary_race, ss = "https://docs.google.com/spreadsheets/d/1REKLjXEztNweS6VUXgqRwKENtQx5Ju17U6NmNOpdtg8/edit?usp=sharing", sheet = "epilepsy_summary_race")
-#write_sheet(epilepsy_summary_sex, ss = "https://docs.google.com/spreadsheets/d/1REKLjXEztNweS6VUXgqRwKENtQx5Ju17U6NmNOpdtg8/edit?usp=sharing", sheet = "epilepsy_summary_sex")
+write_sheet(epilepsy_summary_race, ss = "https://docs.google.com/spreadsheets/d/1UTFaBQiaD4vyVr1mPJwzCm-patal6hx6ORAGal_RAsQ/edit?usp=sharing", sheet = "Epilepsy Race Summary")
+write_sheet(epilepsy_summary_sex, ss = "https://docs.google.com/spreadsheets/d/1UTFaBQiaD4vyVr1mPJwzCm-patal6hx6ORAGal_RAsQ/edit?usp=sharing", sheet = "Epilepsy Sex Summary")
 
 # ----------------------------------------------------------------------
 # Hot-encoding ANOVA and chi-square analysis for race (>2 subgroups)
 # ----------------------------------------------------------------------
-# 
+
 # Create  empty list to store results
 epilepsy_stats_race_list <- list()
 
 # Loop through each explanatory variable
 for (variable in names(epilepsy_df)) {
   # Skip the response variable and non-explanatory variables
-  if (variable %in% c("race")) {
+  if (variable %in% c("RACE_NEW")) {
     next
   }
 
@@ -108,7 +180,7 @@ for (variable in names(epilepsy_df)) {
     # Check if the variable is numeric
     if (variable %in% numeric_variables) {
       # Perform ANOVA
-      anova_result <- aov(epilepsy_df[[variable]] ~ race, data = epilepsy_df)
+      anova_result <- aov(epilepsy_df[[variable]] ~ RACE_NEW, data = epilepsy_df)
       
       # Extract the p-value from ANOVA summary
       p_value <- summary(anova_result)[[1]]$`Pr(>F)`[1]
@@ -119,7 +191,7 @@ for (variable in names(epilepsy_df)) {
       # Perform chi-square test for each level
       levels <- unique(epilepsy_df[[variable]])
       for (level in levels) {
-        contingency_table <- table(epilepsy_df[[variable]] == level, epilepsy_df$race)
+        contingency_table <- table(epilepsy_df[[variable]] == level, epilepsy_df$RACE_NEW)
         chi_square_result <- chisq.test(contingency_table)
         p_value <- chi_square_result$p.value
         
@@ -146,7 +218,7 @@ epilepsy_stats_sex_list <- list()
 # Loop through each explanatory variable
 for (variable in names(epilepsy_df)) {
   # Skip the response variable and non-explanatory variables
-  if (variable %in% c("sex")) {
+  if (variable %in% c("SEX")) {
     next
   }
 
@@ -154,7 +226,7 @@ for (variable in names(epilepsy_df)) {
     # Check if the variable is numeric
     if (variable %in% numeric_variables) {
       # Perform unpaired t-test
-      t_test_result <- t.test(epilepsy_df[[variable]] ~ sex, data = epilepsy_df)
+      t_test_result <- t.test(epilepsy_df[[variable]] ~ SEX, data = epilepsy_df)
       p_value <- t_test_result$p.value
       
       # Append results to the list
@@ -163,7 +235,7 @@ for (variable in names(epilepsy_df)) {
       # Perform chi-square test for each level
       levels <- unique(epilepsy_df[[variable]])
       for (level in levels) {
-        contingency_table <- table(epilepsy_df[[variable]] == level, epilepsy_df$sex)
+        contingency_table <- table(epilepsy_df[[variable]] == level, epilepsy_df$SEX)
         chi_square_result <- chisq.test(contingency_table)
         p_value <- chi_square_result$p.value
         
@@ -181,5 +253,5 @@ for (variable in names(epilepsy_df)) {
 epilepsy_stats_sex <- do.call(rbind, epilepsy_stats_sex_list)
 
 # Write to the Google drive
-#write_sheet(epilepsy_stats_race, ss = "https://docs.google.com/spreadsheets/d/1REKLjXEztNweS6VUXgqRwKENtQx5Ju17U6NmNOpdtg8/edit?usp=sharing", sheet = "epilepsy_stats_race")
-#write_sheet(epilepsy_stats_sex, ss = "https://docs.google.com/spreadsheets/d/1REKLjXEztNweS6VUXgqRwKENtQx5Ju17U6NmNOpdtg8/edit?usp=sharing", sheet = "epilepsy_stats_sex")
+write_sheet(epilepsy_stats_race, ss = "https://docs.google.com/spreadsheets/d/1UTFaBQiaD4vyVr1mPJwzCm-patal6hx6ORAGal_RAsQ/edit?usp=sharing", sheet = "Epilepsy Race Stats")
+write_sheet(epilepsy_stats_sex, ss = "https://docs.google.com/spreadsheets/d/1UTFaBQiaD4vyVr1mPJwzCm-patal6hx6ORAGal_RAsQ/edit?usp=sharing", sheet = "Epilepsy Sex Stats")
